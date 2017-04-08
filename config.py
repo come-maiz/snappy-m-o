5627deab5078ec05285c2f4bd3f31f3e066355c9
Leo Arias <leo.arias@canonical.com>
2017-04-08 01:18:54 +0000

    Add the plugins dir to the config

--- config.py
+++ config.py
@@ -83,7 +83,7 @@ BOT_DATA_DIR = os.environ.get('SNAP_USER_DATA')
 # locally before publishing it. Note that you can specify only a single
 # directory, however you are free to create subdirectories with multiple
 # plugins inside this directory.
-BOT_EXTRA_PLUGIN_DIR = None
+BOT_EXTRA_PLUGIN_DIR = os.path.join(os.environ.get('SNAP') 'plugins')
 
 # If you use an external backend as a plugin,
 # this is where you tell Errbot where to find it.
